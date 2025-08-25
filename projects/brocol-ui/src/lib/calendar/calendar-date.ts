bddaa296bd15ebfaf6ef6a5e904d8f7ada077c66
César Infante <cesarinfantedocampo@gmail.com>
2025-08-25 16:08:38 +0200

    feat: anade metodo para saber si una fecha se encuentra dentro de un rango de fechas
    #21-implementación-role-grid-para-interacción-mediante-teclado

--- projects/brocol-ui/src/lib/calendar/calendar-date.ts
+++ projects/brocol-ui/src/lib/calendar/calendar-date.ts
@@ -72,6 +72,11 @@ export class CalendarDate {
     return this._date.getTime() == date.getTime();
   }
 
+  isInRange(dates: CalendarDate[]) {
+    return dates.find((date)=>this.isSameOrAfter(date.getFirstDayOfMonth()) && this.isSameOrBefore(date.getLastDayOfMonth()));
+  }
+
+  // Formatters
   toISOString() {
     return this._date.toISOString();
   }
